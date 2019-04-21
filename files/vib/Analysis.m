1b9b4907cc6cf53eea32503ecf0d43cb32c6c461
RickJacome <48185556+RickJacome@users.noreply.github.com>
2019-04-21 15:59:07 -0500

    Update Analysis.m

--- files/vib/Analysis.m
+++ files/vib/Analysis.m
@@ -1,25 +1,69 @@
-close all; format compact; clc;
-A1x = R1.Ax; A1y = R1.Ay; A1z = R1.Az;
-Ang1x = R1.Angx; Ang1y = R1.Angy; Ang1z = R1.Angz;
+clear all; clc; close all; format compact
+load('R1.mat'); load('R2.mat'); load('R3.mat'); load('R4.mat');
+A1x = R1.AX; A1y = R1.AY; A1z = R1.AZ;
+Ang1x = R1.AngleX; Ang1y = R1.AngleY; Ang1z = R1.AngleZ;
 
-figure(1); 
-plot(A1x); hold on; plot(A1y); plot(A1z);
-plot(Ang1x); plot(Ang1y); plot(Ang1z);
+figure(1);
+plot(A1x,'LineWidth',2); hold on; 
+plot(A1y,'LineWidth',2); plot(A1z,'LineWidth',2);
+plot(Ang1x,'LineWidth',2); plot(Ang1y,'LineWidth',2); 
+plot(Ang1z,'LineWidth',2);
+legend('A_x','A_y','A_z','\theta_x','\theta_y','\theta_z')
+title('Trial 1'); xlabel('Sample');
 
-A2x = R2.Ax; A2y = R2.Ay; A2z = R2.Az;
-Ang2x = R2.Angx; Ang2y = R2.Angy; Ang2z = R2.Angz;
+A2x = R2.AX; A2y = R2.AY; A2z = R2.AZ;
+Ang2x = R2.AngleX; Ang2y = R2.AngleY; Ang2z = R2.AngleZ;
 figure(2); 
-plot(A2x); hold on; plot(A2y); plot(A2z);
-plot(Ang2x); plot(Ang2y); plot(Ang2z);
+plot(A2x,'LineWidth',2); hold on;
+plot(A2y,'LineWidth',2); plot(A2z,'LineWidth',2);
+plot(Ang2x,'LineWidth',2); plot(Ang2y,'LineWidth',2);
+plot(Ang2z,'LineWidth',2);
+legend('A_x','A_y','A_z','\theta_x','\theta_y','\theta_z')
+title('Trial 2'); xlabel('Sample');
 
-A3x = R3.Ax; A3y = R3.Ay; A3z = R3.Az;
-Ang3x = R3.Angx; Ang3y = R3.Angy; Ang3z = R3.Angz;
+A3x = R3.AX; A3y = R3.AY; A3z = R3.AZ;
+Ang3x = R3.AngleX; Ang3y = R3.AngleY; Ang3z = R3.AngleZ;
 figure(3); 
-plot(A3x); hold on; plot(A3y); plot(A3z);
-plot(Ang3x); plot(Ang3y); plot(Ang3z);
+plot(A3x,'LineWidth',2); hold on;
+plot(A3y,'LineWidth',2); plot(A3z,'LineWidth',2);
+plot(Ang3x,'LineWidth',2); plot(Ang3y,'LineWidth',2);
+plot(Ang3z,'LineWidth',2);
+legend('A_x','A_y','A_z','\theta_x','\theta_y','\theta_z')
+title('Trial 3'); xlabel('Sample');
 
-A4x = R4.Ax; A4y = R4.Ay; A4z = R4.Az;
-Ang4x = R4.Angx; Ang4y = R3.Angy; Ang4z = R4.Angz;
+A4x = R4.AX; A4y = R4.AY; A4z = R4.AZ;
+Ang4x = R4.AngleX; Ang4y = R3.AngleY; Ang4z = R4.AngleZ;
 figure(4); 
-plot(A4x); hold on; plot(A4y); plot(A4z);
-plot(Ang4x); plot(Ang4y); plot(Ang4z);
+plot(A4x,'LineWidth',2); hold on;
+plot(A4y,'LineWidth',2); plot(A4z,'LineWidth',2);
+plot(Ang4x,'LineWidth',2); plot(Ang4y,'LineWidth',2);
+plot(Ang4z,'LineWidth',2);
+legend('A_x','A_y','A_z','\theta_x','\theta_y','\theta_z')
+title('Trial 4'); xlabel('Sample');
+
+close all
+
+figure(5); hold on;
+plot(A1x,'LineWidth',2); plot(A2x,'LineWidth',2);
+plot(A3x,'LineWidth',2); plot(A4x,'LineWidth',2);  
+legend('Test 1','Test 2','Test 3','Test 4');
+figure(6); hold on;
+plot(A1y,'LineWidth',2); plot(A2y,'LineWidth',2);
+plot(A3y,'LineWidth',2); plot(A4y,'LineWidth',2);
+legend('Test 1','Test 2','Test 3','Test 4');
+figure(7); hold on;
+plot(A1z,'LineWidth',2); plot(A2z,'LineWidth',2);
+plot(A3z,'LineWidth',2); plot(A4z,'LineWidth',2); 
+legend('Test 1','Test 2','Test 3','Test 4');
+figure(8); hold on;
+plot(Ang1x,'LineWidth',2); plot(Ang2x,'LineWidth',2);
+plot(Ang3x,'LineWidth',2); plot(Ang4x,'LineWidth',2);
+legend('Test 1','Test 2','Test 3','Test 4');
+figure(9); hold on;
+plot(Ang1y,'LineWidth',2); plot(Ang2y,'LineWidth',2);
+plot(Ang3y,'*','LineWidth',2); plot(Ang4y,'LineWidth',2);
+legend('Test 1','Test 2','Test 3','Test 4');
+figure(10); hold on;
+plot(Ang1z,'LineWidth',2); plot(Ang2z,'LineWidth',2);
+plot(Ang3z,'LineWidth',2); plot(Ang4z,'LineWidth',2); 
+legend('Test 1','Test 2','Test 3','Test 4');
