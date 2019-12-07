add8027b7aba5aa5285868388d1d3f7f7a800772
RickJacome <48185556+RickJacome@users.noreply.github.com>
2019-12-07 16:58:45 -0600

    Create WaveletsCoeff.m

---
+++ files/wavelets/WaveletsCoeff.m
@@ -0,0 +1,19 @@
+close all; clc; clear all
+load('SGT.mat'); load('SGA.mat')
+SGA = SpliGravelAccel; SGT = SplitGravelTime;
+ts = abs(SGT(1) - SGT(2));
+fr = 1/ts;
+plot(SGT,SGA); xlabel('sec'); ylabel('gs'); 
+grid on; title('Data')
+% The CWT is obtained using the analytic 
+%  Morse wavelet with the symmetry parameter
+% (gamma) equal to 3 and the time-bandwidth 
+% product equal to 60
+figure; [wt,f,coi] = cwt(SGA,fr);
+cwt(SGA,fr)
+figure; [wt,periods,coi] = cwt(SGA,seconds(ts));
+cwt(SGA,seconds(ts))
+figure; cwt(SGA,'morse');
+colormap jet; colorbar;
+figure; cwt(SGA,1:128,'haar','plot');
+colormap jet; colorbar;
