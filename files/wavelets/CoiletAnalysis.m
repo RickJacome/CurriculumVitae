6306969803d0435f7ca8d932dcdd33cf1c7f3864
RickJacome <48185556+RickJacome@users.noreply.github.com>
2019-12-07 16:57:43 -0600

    Create CoiletAnalysis.m

---
+++ files/wavelets/CoiletAnalysis.m
@@ -0,0 +1,21 @@
+clc; close all; clear all
+wname = 'coif2';
+f = coifwavf(wname);
+plot(f); grid on
+title('Coiflet 2 Scaling Coefficients')
+ylabel('Amplitude'); xlabel('Section Series')
+
+load('ApproxCoif2L6.mat'); load('SGT.mat');
+Time = SplitGravelTime;
+figure; subplot(211)
+Signal = ApproxCoif2L6(1,:);
+Approx = ApproxCoif2L6(6,:);
+plot(Time(1:numel(Time)/2),Signal(1:numel(Signal)/2)); hold on
+xlabel('Time (sec)'); ylabel('Acceleration (Gs)'); grid on
+title('Raw Longitudinal Vehicle Acceleration')
+xlim([15 100])
+subplot(212)
+plot(Time(1:numel(Time)/2),Approx(1:numel(Signal)/2))
+xlabel('Time (sec)'); ylabel('Acceleration (Gs)'); grid on;
+title('Filtered Longitudinal Vehicle Acceleration')
+ylim([-1 .5]); xlim([15 100])
