93aac2edbc790977f48b566f33abee9246626514
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-02-23 16:34:00 -0600

    Update MultipleCoiflets.m

--- files/wavelets/MultipleCoiflets.m
+++ files/wavelets/MultipleCoiflets.m
@@ -3,12 +3,46 @@ close all; clear all
 % First Obtaining a wavelet decomposition from the 
 % Wavelet Analyzer Toolbox using coiflet 2 at level 6. 
 % Then, just run Functions to see results.
+Thrs = 0.1; WdwSize = 100;
+% ---------------------------------------
+% Split Gravel Testing SGA et SGT
 load('ApproxCoif2L6CompleteSignal.mat'); 
 load('SGT.mat');
-t = SplitGravelTime;
-sig = DecomCoif2L6Complete;
+t = SplitGravelTime; sig = DecomCoif2L6Complete;
+
 CoifPlotter(t,sig);
-Thrs = 0.1; WdwSize = 100;
+FilterSwitch(Thrs,WdwSize,sig,t)
+% ---------------------------------------
+% Split Sand Testing SSA et SST
+load('ApproxCoif2L6CompleteSignalSSA.mat');
+load('SST.mat');
+t = SplitSandTime;  sig = DecomCoif2L6CompSSA;
+
+CoifPlotter(t,sig);
+FilterSwitch(Thrs,WdwSize,sig,t)
+% ---------------------------------------
+% Baseline Full Concrete Testing FCA et FCT
+load('ApproxCoif2L6CompleteSignalFCA.mat');
+load('FCT.mat');
+t = FullConcreteTime;  sig = DecomCoif2L6CompFCA;
+
+CoifPlotter(t,sig);
 FilterSwitch(Thrs,WdwSize,sig,t)
 
+% ---------------------------------------
+% Full Gravel Testing FGA et FGT
+load('ApproxCoif2L6CompleteSignalFGA.mat');
+load('FGT.mat');
+t = FullGravelTime;  sig = DecomCoif2L6CompFGA;
+
+CoifPlotter(t,sig);
+FilterSwitch(Thrs,WdwSize,sig,t)
+% ---------------------------------------
+% Full Sand Testing FSA et FST
+load('ApproxCoif2L6CompleteSignalFSA.mat');
+load('FST.mat');
+t = FullSandTime;  sig = DecomCoif2L6CompFSA;
+
+CoifPlotter(t,sig);
+FilterSwitch(Thrs,WdwSize,sig,t)
 
