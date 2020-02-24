369b80630702f57b168236d578777857ae309e03
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-02-23 23:18:57 -0600

    Update DiscInteg.m

--- files/wavelets/DiscInteg.m
+++ files/wavelets/DiscInteg.m
@@ -1,8 +1,12 @@
-function [Th,n] = DiscInteg(X,Y)
+function [Th,TT,n] = DiscInteg(X,Y)
 K = Y;  S = X;
 n = numel(S);
-Th  = zeros(1,n);  
-for k = 2:n-1 
-   Th(k+1) =  ( (S(k)-S(k-1))*((K(k)+K(k-1))/2) ) + Th(k-1) ; 
-end
+Th  = zeros(1,n); 
+TT = zeros(1,n);
+    for k = 2:n-1    
+        Th(k+1) =  ( (S(k)-S(k-1))*((K(k)+K(k-1))/2) ) + Th(k-1) ; 
+    end
+    for k = 2:n-1 
+       TT(k+1) =  ( (S(k)-S(k-1))*((Th(k)+Th(k-1))/2) ) + TT(k-1) ; 
+    end
 end
