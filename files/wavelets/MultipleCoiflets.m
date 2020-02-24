0c1cd0e2ee3a344334546453a19955f039e58d67
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-02-23 23:51:47 -0600

    Update MultipleCoiflets.m

--- files/wavelets/MultipleCoiflets.m
+++ files/wavelets/MultipleCoiflets.m
@@ -1,5 +1,4 @@
-close all; clear all
-% Multiple Analysis of Coefficients of Friction with Coiflet 2 
+close all; clear all% Multiple Analysis of Coefficients of Friction with Coiflet 2 
 % First Obtaining a wavelet decomposition from the 
 % Wavelet Analyzer Toolbox using coiflet 2 at level 6. 
 % Then, just run Functions to see results.
@@ -8,9 +7,13 @@ Thrs = 0.1; WdwSize = 100;
 % Split Gravel Testing SGA et SGT
 load('ApproxCoif2L6CompleteSignal.mat'); 
 load('SGT.mat'); load('SGA.mat')
+a = SpliGravelAccel;
 t = SplitGravelTime; sig = DecomCoif2L6Complete;
 CoifPlotter(t,sig);
 FilterSwitch(Thrs,WdwSize,sig,t)
+[v,d,n] = DiscInteg(t,a*9.81);
+figure; plot(t,v); xlabel('Time (sec)'); ylabel('Velocity m/s')
+figure; plot(t,d); xlabel('Time (sec)'); ylabel('Distance m')
 % ---------------------------------------
 % Split Sand Testing SSA et SST
 load('ApproxCoif2L6CompleteSignalSSA.mat');
