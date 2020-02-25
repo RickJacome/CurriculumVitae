3341dd2ba86e291af090a81f013c65099d06e4c9
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-02-24 18:18:07 -0600

    Update FCAetFCT.m

--- files/wavelets/FCAetFCT.m
+++ files/wavelets/FCAetFCT.m
@@ -7,10 +7,24 @@ load('FCT.mat'); load('FCA.mat')
 t = FullConcreteTime;  sig = DecomCoif2L6CompFCA;
 a = FullConcreteAccel; 
 CoifPlotter(t,sig); 
-FilterSwitch(Thrs,WdwSize,sig,t)
-[v,d,n] = DiscInteg(t,a*9.81);
+[FricCoeff] = FilterSwitch(Thrs,WdwSize,sig,t);
+% ---------------------------------------
+[v,d,n] = DiscIntegSims(t,a*9.81);
 figure; plot(t,v); grid on;
 xlabel('Time (sec)'); ylabel('Velocity m/s');
 figure; plot(t,d); grid on;
 xlabel('Time (sec)'); ylabel('Distance m')
 % ---------------------------------------
+% From Literature, 
+% Concrete Coeff Friction Sliding Value = 0.55
+% Select Appropiate Ranges for Braking Incident
+% First Event
+idx1 = find(t ==38); idx2 = find(t ==42);
+% Second Event
+idx3 = find(t ==157); idx4 = find(t ==163);
+% The following inputs, the Range of Braking Incident
+% The Integrated Velocity, and the FricCoeff from Filter Switch
+[FGmu1,FGmu2] = FrictionEst(FricCoeff,v,idx1,idx2,idx3,idx4)
+LitValue = 1;
+Relative_Error1 = abs((FGmu1 - LitValue))/LitValue*100
+Relative_Error2 = abs((FGmu2 - LitValue))/LitValue*100
