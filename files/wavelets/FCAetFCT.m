0efabf858a14623c8fabc83d4697caa6a179e35e
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-14 18:14:00 -0500

    Update FCAetFCT.m

--- files/wavelets/FCAetFCT.m
+++ files/wavelets/FCAetFCT.m
@@ -10,11 +10,13 @@ figure; plot(t,a*-1); xlabel('Time (sec)'); ylabel('Acceleration (Gs)')
 CoifPlotter(t,sig); 
 [FricCoeff] = FilterSwitch(Thrs,WdwSize,sig,t);
 % ---------------------------------------
-[v,d,n] = DiscIntegSims(t,a*9.81);
-figure; plot(t,v); grid on;
-xlabel('Time (sec)'); ylabel('Velocity m/s');
-figure; plot(t,d); grid on;
-xlabel('Time (sec)'); ylabel('Distance m')
+% Discrete Integration of Data not needed. It is resolved on 
+% Drifting Removal data files.
+% [v,d,n] = DiscIntegSims(t,a*9.81);
+% figure; plot(t,v); grid on;
+% xlabel('Time (sec)'); ylabel('Velocity m/s');
+% figure; plot(t,d); grid on;
+% xlabel('Time (sec)'); ylabel('Distance m')
 
 % ---------------------------------------
 % From Literature, 
@@ -26,8 +28,8 @@ idx1 = find(t ==38); idx2 = find(t ==42);
 idx3 = find(t ==157); idx4 = find(t ==163);
 % The following inputs, the Range of Braking Incident
 % The Integrated Velocity, and the FricCoeff from Filter Switch
-[FGmu1,FGmu2] = FrictionEst(FricCoeff,v,idx1,idx2,idx3,idx4)
+[FGmu1,FGmu2] = FrictionEst(FricCoeff,t,idx1,idx2,idx3,idx4)
 %http://hpwizard.com/tire-friction-coefficient.html
-LitValue = 0.90;
+LitValue = 0.9305;
 Relative_Error1 = abs((FGmu1 - LitValue))/LitValue*100
 Relative_Error2 = abs((FGmu2 - LitValue))/LitValue*100
