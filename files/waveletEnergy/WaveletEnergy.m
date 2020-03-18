00cb7e70c243766195b9efa40d3f226966418a41
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-17 23:54:55 -0500

    Update WaveletEnergy.m

--- files/waveletEnergy/WaveletEnergy.m
+++ files/waveletEnergy/WaveletEnergy.m
@@ -1,10 +1,17 @@
 %%Coilet Analysis 
 clc; close all; clear all
 load('FCA.mat')
+format compact
 Signal = FullConcreteAccel;
-%Energy Shannon Ratio for level 6;
-[R] = EnergyShanonRatio(Signal,'coif2')
-[R] = EnergyShanonRatio(Signal,'coif1')
-[R] = EnergyShanonRatio(Signal,'haar')
-[R] = EnergyShanonRatio(Signal,'db1')
-[R] = EnergyShanonRatio(Signal,'dmey')
+
+for k = 1:6
+        coi(k) = EnergyShanonRatio(Signal,k,'coif1');
+        ha(k) = EnergyShanonRatio(Signal,k,'haar');
+        db(k) = EnergyShanonRatio(Signal,k,'db1');
+        dm(k) = EnergyShanonRatio(Signal,k,'dmey');
+end
+    R = [coi; ha; db; dm]
+%  EnergyShanonRatio(Signal,5,'coif1')
+%  EnergyShanonRatio(Signal,5,'haar')
+%  EnergyShanonRatio(Signal,5,'db1')
+%  EnergyShanonRatio(Signal,5,'dmey')
