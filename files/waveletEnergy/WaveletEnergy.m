ab461dd0a3395a836e4b9f3446f4b733e3602e25
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-18 13:21:07 -0500

    Update WaveletEnergy.m

--- files/waveletEnergy/WaveletEnergy.m
+++ files/waveletEnergy/WaveletEnergy.m
@@ -3,15 +3,26 @@ clc; close all; clear all
 load('FCA.mat')
 format compact
 Signal = FullConcreteAccel;
+s = 1e2; wv = 'db2';
+MaxDecompLevel = wmaxlev(s,wv)
 
-for k = 1:6
-        coi(k) = EnergyShanonRatio(Signal,k,'coif1');
+for k = 1:MaxDecompLevel
         ha(k) = EnergyShanonRatio(Signal,k,'haar');
-        db(k) = EnergyShanonRatio(Signal,k,'db1');
+        coi(k) = EnergyShanonRatio(Signal,k,'coif2');
+        db(k) = EnergyShanonRatio(Signal,k,'db2');
         dm(k) = EnergyShanonRatio(Signal,k,'dmey');
+        sy(k) = EnergyShanonRatio(Signal,k,'sym2');        
 end
-    R = [coi; ha; db; dm]
-%  EnergyShanonRatio(Signal,5,'coif1')
-%  EnergyShanonRatio(Signal,5,'haar')
-%  EnergyShanonRatio(Signal,5,'db1')
-%  EnergyShanonRatio(Signal,5,'dmey')
+    R = [ha; coi; db; dm; sy]
+T = table(abs(ha'), abs(coi'), abs(db'), abs(dm'), abs(sy'));
+filename = 'DecompLevelEnergy.xlsx';
+writetable(T,filename,'Sheet',1,'Range','B1')
+% The highest Level of Energy Ratio is Coiflet
+
+% [ERatio1] = RelativeEnergy(Signal,'coif1')
+% [ERatio2] = RelativeEnergy(Signal,'coif2')
+% [ERatio3] = RelativeEnergy(Signal,'coif3')
+% [ERatio4] = RelativeEnergy(Signal,'coif4')
+% [ERatio5] = RelativeEnergy(Signal,'coif5')
+
+
