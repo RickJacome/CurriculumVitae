0896e1734e63cda13ba5d7372774cacde1035d20
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-25 17:36:18 -0500

    Update Notes.m

--- files/wavelets/matlabData/Slice/Notes.m
+++ files/wavelets/matlabData/Slice/Notes.m
@@ -2,6 +2,57 @@
 % To extract data, I used the Slice data in Excel for Data analysis
 % Name Convention for different Accelerations
 % Column in Excel = Name in Matlab Folder
+% Excel File
+% ABS-2 SLICE1 - Accelerometer Analysis
 %Column V = FCA1CFC180avec10msAvgAccelx
 %Column H = FCA1ZeroCFC180Accelx
 %Column C = FCA1Accelx
+%Column B = FCT1Slc
+% Excel File
+% ABS-3 SLICE1 - Accelerometer Analysis
+%Column V = FCA2CFC180avec10msAvgAccelx
+%Column H = FCA2ZeroCFC180Accelx
+%Column C = FCA2Accelx
+%Column B = FCT2Slc
+% Excel File
+% ABS-4 SLICE1 - Accelerometer Analysis
+%Column V = FGA1CFC180avec10msAvgAccelx
+%Column H = FGA1ZeroCFC180Accelx
+%Column C = FGA1Accelx
+%Column B = FGT1Slc
+% Excel File
+% ABS-5 SLICE1 - Accelerometer Analysis
+%Column V = FGA2CFC180avec10msAvgAccelx
+%Column H = FGA2ZeroCFC180Accelx
+%Column C = FGA2Accelx
+%Column B = FGT2Slc
+% ABS-6 SLICE1 - Accelerometer Analysis
+%Column V = FSA1CFC180avec10msAvgAccelx
+%Column H = FSA1ZeroCFC180Accelx
+%Column C = FSA1Accelx
+%Column B = FST1Slc
+% ABS-7 SLICE1 - Accelerometer Analysis
+%Column V = FSA2CFC180avec10msAvgAccelx
+%Column H = FSA2ZeroCFC180Accelx
+%Column C = FSA2Accelx
+%Column B = FST2Slc
+% ABS-8 SLICE1 - Accelerometer Analysis
+%Column V = SGA1CFC180avec10msAvgAccelx
+%Column H = SGA1ZeroCFC180Accelx
+%Column C = SGA1Accelx
+%Column B = SGT1Slc
+% ABS-9 SLICE1 - Accelerometer Analysis
+%Column V = SGA2CFC180avec10msAvgAccelx
+%Column H = SGA2ZeroCFC180Accelx
+%Column C = SGA2Accelx
+%Column B = SGT2Slc
+% ABS-10 SLICE1 - Accelerometer Analysis
+%Column V = SSA1CFC180avec10msAvgAccelx
+%Column H = SSA1ZeroCFC180Accelx
+%Column C = SSA1Accelx
+%Column B = SST1Slc
+% ABS-11 SLICE1 - Accelerometer Analysis
+%Column V = SSA2CFC180avec10msAvgAccelx
+%Column H = SSA2ZeroCFC180Accelx
+%Column C = SSA2Accelx
+%Column B = SST2Slc
