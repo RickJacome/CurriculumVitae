457a41aaa6b8b7295d1d5bf434fdb4058a97d3fd
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-25 13:40:13 -0500

    Create Notes.m

---
+++ files/wavelets/matlabData/Slice/Notes.m
@@ -0,0 +1,7 @@
+%Navigation Notes:
+% To extract data, I used the Slice data in Excel for Data analysis
+% Name Convention for different Accelerations
+% Column in Excel = Name in Matlab Folder
+%Column V = FCA1CFC180avec10msAvgAccelx
+%Column H = FCA1ZeroCFC180Accelx
+%Column C = FCA1Accelx
