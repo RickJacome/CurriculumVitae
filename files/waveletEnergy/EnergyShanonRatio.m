5794759035d34fc9baba44be421c33446978e7d9
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-26 17:35:08 -0500

    Update EnergyShanonRatio.m

--- files/waveletEnergy/EnergyShanonRatio.m
+++ files/waveletEnergy/EnergyShanonRatio.m
@@ -2,7 +2,7 @@ function [Ratio] = EnergyShanonRatio(signal,level,type)
 [C,L] = wavedec(signal,level,type);
 [~,Ed] = wenergy(C,L);
 % Ed = E(n)
-E = wentropy(C,'shannon');
+E = wentropy(signal,'shannon');
 
 Ratio = sum(abs(Ed).^2)/-1*E;
 end
