a9b69e76ac10b0cdcb8a14f37bb7bc36a527e69a
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-25 23:18:31 -0500

    Update Trial.m

--- files/wavelets/Trial.m
+++ files/wavelets/Trial.m
@@ -1,23 +1,32 @@
 clc; close all; clear all
 load('matlabData\Slice\FCA1Accelx.mat'); 
-plot(FCA1Accelx)
+load('matlabData\Slice\FCT1Slc.mat');
+plot(FCT1Slc,FCA1Accelx)
 
 %Slice Signal 1000 Hz
 figure
-Signal = FCA1Accelx-2.3; Signal(isnan(Signal))=[];
+y = mean(FCA1Accelx(1:1000));
+Signal = FCA1Accelx-y;
+idx = isnan(Signal);
+Signal(idx == 1)=[];
+
+%FCT1Slc = FCT1Slc(idx);
+
 [C,L] = wavedec(Signal,6,'coif2');
 approx = appcoef(C,L,'coif2');
 plot(approx); 
 
 title('Coiflet2 Approximation Slice')
-figure
+hold on;
 [C,L] = wavedec(Signal,9,'haar');
 approx = appcoef(C,L,'haar');
 plot(approx); title('Haar Approximation Slice')
-
+legend('Coiflet 2 Approximation','Haar Approximation')
 
 %VC4000 Signal 100 Hz
-load('matlabData\FCA.mat'); 
+load('matlabData\FCA.mat');
+load('matlabData\FCT.mat');
+
 Signal = FullConcreteAccel; 
 Signal = Signal(1:(numel(Signal)-1)/2);
 figure
@@ -25,8 +34,9 @@ figure
 approx = appcoef(C,L,'coif2');
 plot(approx); title('Coiflet2 Approximation VC4000')
 
-figure
+figure;
 [C,L] = wavedec(Signal,6,'haar');
 approx = appcoef(C,L,'haar');
 plot(approx); title('Haar Approximation VC4000')
+legend('Coiflet 2 Approximation','Haar Approximation')
 
