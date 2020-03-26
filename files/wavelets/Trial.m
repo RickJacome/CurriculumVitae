09af011ad93c4684c6075c36806b875db95e5c4b
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-25 20:46:02 -0500

    Update Trial.m

--- files/wavelets/Trial.m
+++ files/wavelets/Trial.m
@@ -1,10 +1,15 @@
 clc; close all; clear all
 load('matlabData\Slice\FCA1Accelx.mat'); 
+plot(FCA1Accelx)
+
 %Slice Signal 1000 Hz
-Signal = FCA1Accelx; Signal(isnan(Signal))=[];
+figure
+Signal = FCA1Accelx-2.3; Signal(isnan(Signal))=[];
 [C,L] = wavedec(Signal,6,'coif2');
 approx = appcoef(C,L,'coif2');
-plot(approx); title('Coiflet2 Approximation Slice')
+plot(approx); 
+
+title('Coiflet2 Approximation Slice')
 figure
 [C,L] = wavedec(Signal,9,'haar');
 approx = appcoef(C,L,'haar');
@@ -13,7 +18,8 @@ plot(approx); title('Haar Approximation Slice')
 
 %VC4000 Signal 100 Hz
 load('matlabData\FCA.mat'); 
-Signal = FullConcreteAccel;
+Signal = FullConcreteAccel; 
+Signal = Signal(1:(numel(Signal)-1)/2);
 figure
 [C,L] = wavedec(Signal,3,'coif2');
 approx = appcoef(C,L,'coif2');
