a0ec2cd090efa0f4cd4a7330948850d12f94e6c7
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-03-30 23:42:00 -0500

    Update GradTrapezoid.m

--- files/SAE/GradTrapezoid.m
+++ files/SAE/GradTrapezoid.m
@@ -5,7 +5,6 @@ syms x1 x2 x3 x5 s
 eq1 = (x5/(x2-x1))*s - x1*x5/(x2-x1);
 f1 = eq1*(heaviside(s-x1) - heaviside(s-x2));
 f2 = x5*(heaviside(s-x2)-heaviside(s-x3));
-
 f = f1 + f2;
 gradient(f,[x1 x2 x3 x5])
 %%% Gradient of Complete Trapezoid
