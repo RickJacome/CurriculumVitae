0a186818e34d9ef2c00c7cc9074948d3f68a741a
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-04-12 00:09:10 -0500

    Update CurveFitlsq.m

--- files/SAE/CurveFitlsq.m
+++ files/SAE/CurveFitlsq.m
@@ -15,12 +15,16 @@ times = linspace(s(1),s(end));
 hold on; plot(s,y,'ko',times,fun1(x,times),'b-')
 legend('Data','Fitted Response'); title('Data and Fitted Curve')
 %--------------------------------------------------------
-% Half Trapezoid   
-s = [1 2 3 5 6 7 8];
-y = [0 2 5.4 4.8 5.2 5 5.6];
-fun2 = @(x,s) ((x(5)./(x(2)-x(1))).*s - x(1).*x(5)./(x(2)-x(1))).*(heaviside(s-x(1)) - heaviside(s-x(2))) +...
-     x(5).*(heaviside(s-x(2))-heaviside(s-x(3)));
-% Find the "minimized error".
+%Trapezoid  with factorization 
+s = [1 2 3 5 6 7 8 12];
+y = [0 2 5.4 4.8 5.2 5 5.6 1];
+x0 = [0.9 2.9 4.89 7.85 11.50];
+
+fun2 = @(x,s) ((x(5)./(x(2)-x(1))).*(s - x(1))).*(heaviside(s-x(1)) - heaviside(s-x(2))) +...
+     x(5).*(heaviside(s-x(2))-heaviside(s-x(3))) + ...
+( ( x(5)./(x(4)-x(3))).*(-s+x(3))+ x(5) ).*(heaviside(s-x(3)) - heaviside(s-x(4))); 
+
+ % Find the "minimized error".
 x = lsqcurvefit(fun2,x0,s,y)
 %x = lsqnonlin(fun,x0)
 figure; hold on; plot(s,y,'ko',times,fun2(x,times),'b-')
