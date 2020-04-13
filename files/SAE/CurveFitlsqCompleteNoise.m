cb4f8f885eba157fcf3a8a1b44eeea044872a73f
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-04-12 22:57:26 -0500

    Update CurveFitlsqCompleteNoise.m

--- files/SAE/CurveFitlsqCompleteNoise.m
+++ files/SAE/CurveFitlsqCompleteNoise.m
@@ -2,13 +2,15 @@ clc; clear all; close all
 % Same as CurveFitlsq but uses a higher amount of data points
 % Behavior is good because even though there should be 
 % a "drop" the function compensates and does not generate a drop
-s = 1:.01:12;
+% The only problem is that the function only behaves properly when the data
+% starts at 0 for y-axis
+s = 1:.01:25;
 %y = [0 2 4.8 5.2 5 5.6];
 n = numel(s)-1;
 y1 = 2.*s(1:n/2) - 1;
-y2 = 11*ones(1,n/2);
-y1o = awgn(y1,20,'measured');
-y2o = awgn(y2,20,'measured');
+y2 = 23*ones(1,n/2);
+y1o = awgn(y1,10,'measured');
+y2o = awgn(y2,10,'measured');
 
 y = [y1o y2o];
 x0 = [0.9 2.9 4.89 7.85 11.50];
@@ -19,10 +21,11 @@ fun1 = @(x,s) ((x(5)./(x(2)-x(1))).*s - x(1).*x(5)./(x(2)-x(1))).*(heaviside(s-x
 
 x = lsqcurvefit(fun1,x0,s(1:end-1),y)
 times = linspace(s(1),s(end-1));
-hold on; plot(s(1:end-1),y,'ko')
-plot(times,fun1(x,times),'b-')
+hold on; plot(s(1:end-1),y,'bo')
+plot(times,fun1(x,times),'k-','linewidth',2)
 xlim([times(1), times(end)+5])
-legend('Data','Fitted Response'); title('Data and Fitted Curve')
+legend('Data','Fitted Response','location','best'); 
+title('Data and Fitted Curve'); grid on
 
 fun2 = @(x,s) ((x(5)./(x(2)-x(1))).*(s - x(1))).*(heaviside(s-x(1)) - heaviside(s-x(2))) +...
      x(5).*(heaviside(s-x(2))-heaviside(s-x(3))) + ...
@@ -32,8 +35,9 @@ fun2 = @(x,s) ((x(5)./(x(2)-x(1))).*(s - x(1))).*(heaviside(s-x(1)) - heaviside(
 x = lsqcurvefit(fun1,x0,s(1:end-1),y)
 times = linspace(s(1),s(end-1));
 figure
-hold on; plot(s(1:end-1),y,'ko')
-plot(times,fun2(x,times),'b-')
-legend('Data','Fitted Response'); title('Data and Fitted Curve')
+hold on; plot(s(1:end-1),y,'bo')
+plot(times,fun2(x,times),'k-','linewidth',2)
+legend('Data','Fitted Response','location','best'); 
+title('Data and Fitted Curve'); grid on
 xlim([times(1), times(end)+5])
 
