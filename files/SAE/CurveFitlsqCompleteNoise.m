6062bc3fca05afb4048b1bcf7b546cf143c77629
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-04-13 11:44:13 -0500

    Update CurveFitlsqCompleteNoise.m

--- files/SAE/CurveFitlsqCompleteNoise.m
+++ files/SAE/CurveFitlsqCompleteNoise.m
@@ -4,16 +4,17 @@ clc; clear all; close all
 % a "drop" the function compensates and does not generate a drop
 % The only problem is that the function only behaves properly when the data
 % starts at 0 for y-axis
-s = 1:.01:25;
+s = 1:.01:30;
 %y = [0 2 4.8 5.2 5 5.6];
 n = numel(s)-1;
-y1 = 2.*s(1:n/2) - 1;
-y2 = 23*ones(1,n/2);
-y1o = awgn(y1,10,'measured');
-y2o = awgn(y2,10,'measured');
+y1 = (2.*s(1:n/2) - 3)*1e-3;
+y2 = 26*ones(1,n/2)*1e-3;
+y1o = awgn(y1,25,'measured');
+y2o = awgn(y2,25,'measured');
 
 y = [y1o y2o];
 x0 = [0.9 2.9 4.89 7.85 11.50];
+x0 = [1 2 3 4 5];
 % Find the "minimized error".
 fun1 = @(x,s) ((x(5)./(x(2)-x(1))).*s - x(1).*x(5)./(x(2)-x(1))).*(heaviside(s-x(1)) - heaviside(s-x(2))) +...
      x(5).*(heaviside(s-x(2))-heaviside(s-x(3))) + ...
@@ -25,7 +26,9 @@ hold on; plot(s(1:end-1),y,'bo')
 plot(times,fun1(x,times),'k-','linewidth',2)
 xlim([times(1), times(end)+5])
 legend('Data','Fitted Response','location','best'); 
-title('Data and Fitted Curve'); grid on
+title('Optimization of Curvature Model \kappa_1'); grid on
+xlabel('Segment of Length s'); ylabel('Curvature \kappa');
+ 
 
 fun2 = @(x,s) ((x(5)./(x(2)-x(1))).*(s - x(1))).*(heaviside(s-x(1)) - heaviside(s-x(2))) +...
      x(5).*(heaviside(s-x(2))-heaviside(s-x(3))) + ...
@@ -38,6 +41,7 @@ figure
 hold on; plot(s(1:end-1),y,'bo')
 plot(times,fun2(x,times),'k-','linewidth',2)
 legend('Data','Fitted Response','location','best'); 
-title('Data and Fitted Curve'); grid on
+title('Optimization of Curvature Model \kappa_1'); grid on
+xlabel('Segment of Length s'); ylabel('Curvature \kappa');
 xlim([times(1), times(end)+5])
 
