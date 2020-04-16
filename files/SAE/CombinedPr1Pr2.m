2d15a6609e18362e299415d2b8970c8100789b70
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-04-16 00:49:05 -0500

    Update CombinedPr1Pr2.m

--- files/SAE/CombinedPr1Pr2.m
+++ files/SAE/CombinedPr1Pr2.m
@@ -1,60 +1,77 @@
 clc; clear all; close all
-% This model implements both Pr. 1 and Pr. 2 in one matlab script
-% however, both of the problems are independent (for now).
-% Results coincide with AMPL optimization;
+% This code takes a curvature model (M#) and
+% implements both Pr. 1 and Pr. 2 together.
+% It creates a loop of optimization problems related to the Curvature
+% model provided with Pr. 2. Such that an optimal velocity is found for 
+% every curvature datapoint for any general [s] road. 
+%------------------------------
+% Road Curvature Decomposition Data
 s = 1:.01:25; n = numel(s)-1;
 y1 = (2.*s(1:n/2) - 3)*1e-3;
 y2 = 26*ones(1,n/2)*1e-3;
-
+% Added Gaussian noise, but not needed.
 y1o = awgn(y1,25,'measured'); y2o = awgn(y2,25,'measured');
-
 y = [y1o y2o];
+% Initial Conditions, NEVER repeat them.
 x0 = [0.9 2.9 4.89 7.85 11.50];
-
+% Curvature Model M.1
 M1 = @(x,s) ((x(5)./(x(2)-x(1))).*(s - x(1))).*(heaviside(s-x(1)) - heaviside(s-x(2))) +...
      x(5).*(heaviside(s-x(2))-heaviside(s-x(3))) + ...
 ( ( x(5)./(x(4)-x(3))).*(-s+x(3))+ x(5) ).*(heaviside(s-x(3)) - heaviside(s-x(4))); 
-
-x = lsqcurvefit(M1,x0,s(1:end-1),y)
-times = linspace(s(1),s(end-1),1000); % <--- This defines the 
+% Pr.1
+fprintf('Pr. 1, Least Squares Min. Has finalized');
+options = optimset('Display','off');
+x = lsqcurvefit(M1,x0,s(1:end-1),y,[],[],options)
+snew = linspace(s(1),s(end-1),100); % <--- This defines the 
 % size of the "K_vector".
 figure; hold on; 
 plot(s(1:end-1),y,'bo');
-plot(times,M1(x,times),'k-','linewidth',2);
-xlim([times(1), times(end)+5]);
+plot(snew,M1(x,snew),'k-','linewidth',2);
+xlim([snew(1), snew(end)+5]);
 legend('Data','Fitted Response','location','best'); 
 title('Data and Fitted Curve'); grid on
 
 % -------------------------
 %Parameters 
 global K_temp e g mu U
-L = 2.5; 
-K_vector = M1(x,times);
-%K_vector = 0.0167;
-U = 1.95; g = 9.81;  e = 6; mu = 0.4;
+% Vehicle Only
+L = 2.5;  U = 1.95;
+% Road Only
+e = 6; mu = 0.4;
+% Both
+g = 9.81; K_vector = M1(x,snew);
 % -------------------------
+%Iterative Optimization Routine for Pr.2 given Optimized M.1
 for i = 1:length(K_vector)
 K_temp = K_vector(i);  
-% Objective Function 
+% Objective Function Pr.2
 fun = @(x)  x(1) - (53.7*L + U*x(2)^2/g)*K_temp;    
+%C.1 (Bounds)
 lb = [-3,0]; % -3 < x1 < 3;
 ub = [3,60];  % 0 < x2 < 60;
 % There are no linear constraints, so set those arguments to |[]|. 
-A = [];  b = [];
-Aeq = []; beq = [];  
+A = [];  b = []; % Linear In-equality Constraints
+Aeq = []; beq = [];  % Linear Equality Constraints
 %Initial Conditions
 x0 = [1/4,1/4];  
 %Constraints as an annoynomous function
 nonlcon = @EqConstraint;
-
-
-Op(i,:) = fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon);  
+options = optimoptions('fmincon','Display','off');
+Op(i,:) = fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon,options);  
 end
-figure; plot(Op(:,2))
-title('Velocity Optimized'); grid on
+fprintf('Pr. 2 Has finalized \n');
+figure; plot(snew,Op(:,2))
+title('Segment Length vs Velocity Optimized'); grid on
+figure; plot(M1(x,snew),Op(:,2))
+title('Curvature vs Velocity Optimized'); grid on
+
+% Nonlinear Constaints (Not bounds)
 function [c,ceq] = EqConstraint(x)
 global K_temp e g mu 
+%Pr.2
+% Nonlinear Inequality Constraints
 c = x(2)^2*K_temp/g - (mu + 0.01*e)/(1-0.01*mu*e);
+% Nonlinear Equality Constraints
 ceq = [];
 end
 
