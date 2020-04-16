fee7905fd26a7259444ff90f9797122661f17793
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-04-16 17:38:09 -0500

    Create CurvatureOptimization.m

---
+++ files/SAE/CurvatureOptimization.m
@@ -0,0 +1,89 @@
+clear; close all; clc
+%Debugging, needs to find correct numbers. 
+%GPS DATA
+%load('CVF9LatX.mat'); load('CVF9LongY.mat');
+%Ideal AASHTO
+load('MichXm.mat'); load('MichYm.mat');
+%x2 = LatX'; y2 = LongY';   
+x2 = xm'; y2 = ym';
+x2 = unique(x2); y2 = unique(y2);
+x2 = x2(1:numel(y2));
+X = [x2',y2'];
+[L,R,K] = curvature(X);
+K(1,:) = []; K(end,:) = [];
+L(1,:) = []; L(end,:) = [];
+xlabel('Length of Road'); ylabel('Radius \rho')
+figure(1);
+x2(1) = []; x2(end) = [];
+y2(1) = []; y2(end) = [];
+h = plot(x2,y2); grid on; axis equal; set(h,'marker','.');
+xlabel('X Coordinate'); ylabel('Y Coordinate')
+title('Road with Curvature Vectors')
+hold on
+quiver(x2',y2',K(:,1),K(:,2)); hold off  
+y = sqrt(K(:,1).^2 + K(:,2).^2);
+s = L;
+% Initial Conditions, NEVER repeat them.
+x0 = [100 200 300 400 500];
+% Curvature Model M.1
+M1 = @(x,s) ((x(5)./(x(2)-x(1))).*(s - x(1))).*(heaviside(s-x(1)) - heaviside(s-x(2))) +...
+     x(5).*(heaviside(s-x(2))-heaviside(s-x(3))) + ...
+( ( x(5)./(x(4)-x(3))).*(-s+x(3))+ x(5) ).*(heaviside(s-x(3)) - heaviside(s-x(4))); 
+% Pr.1
+fprintf('Pr. 1, Least Squares Min. Has finalized');
+options = optimset('Display','off');
+x = lsqcurvefit(M1,x0,s(1:end),y,[],[],options)
+snew = linspace(s(1),s(end),100); % <--- This defines the 
+% size of the "K_vector".
+figure; hold on; 
+plot(s,y,'bo');
+plot(snew,M1(x,snew),'k-','linewidth',2);
+xlim([snew(1), snew(end)+5]);
+legend('Data','Fitted Response','location','best'); 
+title('Data and Fitted Curve'); grid on
+
+% -------------------------
+%Parameters 
+global K_temp e g mu U
+% Vehicle Only
+L = 2.5;  U = 1.95;
+% Road Only
+e = 12; mu = 0.4;
+% Both
+g = 9.81; K_vector = M1(x,snew);
+% -------------------------
+%Iterative Optimization Routine for Pr.2 given Optimized M.1
+for i = 1:length(K_vector)
+K_temp = K_vector(i);  
+% Objective Function Pr.2
+fun = @(x)  x(1) - (53.7*L + U*x(2)^2/g)*K_temp;    
+%C.1 (Bounds)
+lb = [-3,25]; % -3 < x1 < 3;
+ub = [3,60];  % 60 < x2 < 80; mph
+% There are no linear constraints, so set those arguments to |[]|. 
+A = [];  b = []; % Linear In-equality Constraints
+Aeq = []; beq = [];  % Linear Equality Constraints
+%Initial Conditions
+x0 = [1/4,1/4];  
+%Constraints as an annoynomous function
+nonlcon = @EqConstraint;
+options = optimoptions('fmincon','Display','off');
+Op(i,:) = fmincon(fun,x0,A,b,Aeq,beq,lb,ub,nonlcon,options);  
+end
+fprintf('Pr. 2 Has finalized \n');
+figure; plot(snew,Op(:,2))
+title('Segment Length vs Velocity Optimized'); grid on
+figure; plot(M1(x,snew),Op(:,2))
+title('Curvature vs Velocity Optimized'); grid on
+
+% Nonlinear Constaints (Not bounds)
+function [c,ceq] = EqConstraint(x)
+global K_temp e g mu 
+%Pr.2
+% Nonlinear Inequality Constraints
+c = x(2)^2*K_temp/g - (mu + 0.01*e)/(1-0.01*mu*e);
+% Nonlinear Equality Constraints
+ceq = [];
+end
+
+
