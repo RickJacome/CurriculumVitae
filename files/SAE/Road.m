86a593f767a7136c1c5013d788be22b5004fb957
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-06-17 11:24:37 -0500

    Create Road.m

---
+++ files/SAE/Road.m
@@ -0,0 +1,43 @@
+clear; close all; clc
+%GPS DATA
+load('CVF9LatX.mat'); load('CVF9LongY.mat');
+x2 = LatX'; y2 = LongY';   
+%x2 = unique(x2); y2 = unique(y2);
+x2 = x2(1:numel(y2));
+X = [x2',y2'];
+[L,R,K] = curvature(X);
+K(1,:) = []; K(end,:) = [];
+L(1,:) = []; L(end,:) = [];
+xlabel('Length of Road'); ylabel('Radius \rho')
+figure(1);
+x2(1) = []; x2(end) = [];
+y2(1) = []; y2(end) = [];
+h = plot(x2,y2); grid on; axis equal; set(h,'marker','.');
+xlabel('X Coordinate'); ylabel('Y Coordinate')
+title('Road with Curvature Vectors')
+hold on
+quiver(x2',y2',K(:,1),K(:,2)); hold off  
+
+x2 = unique(x2,'stable'); y2 = unique(y2,'stable');
+x2 = x2(1:numel(y2));
+X = [x2',y2'];
+[L,R,K] = curvature(X);
+K(1,:) = []; K(end,:) = [];
+L(1,:) = []; L(end,:) = [];
+xlabel('Length of Road'); ylabel('Radius \rho')
+figure(2);
+x2(1) = []; x2(end) = [];
+y2(1) = []; y2(end) = [];
+h = plot(x2,y2); grid on; axis equal; set(h,'marker','.');
+xlabel('X Coordinate'); ylabel('Y Coordinate')
+title('Road with Curvature Vectors')
+hold on
+quiver(x2',y2',K(:,1),K(:,2)); hold off  
+
+
+
+
+
+
+
+
