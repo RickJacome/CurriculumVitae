6010ff47a805e4fe716a0e0eb19ecf61d7ad3909
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-11-02 14:52:32 -0600

    Create Trapezoid.m

---
+++ files/SAE/aOther Code/Trapezoid.m
@@ -0,0 +1,32 @@
+% Trapezoid
+close all; clear all; clc
+x1 = 2; x2 = 6;
+x3 = 12; x4 = 21;
+x5 = 7;
+syms s
+theta = atand((x2-x1)/x5);
+eq1 = (x5/(x2-x1))*s - x1/tand(theta);
+f1 = eq1*(heaviside(s-x1) - heaviside(s-x2));
+
+f2 = x5*(heaviside(s-x2)-heaviside(s-x3));
+
+alpha = atand((x4-x3)/x5);
+eq2 = (x5/(x4-x3))*-s + ( (x3)/tand(alpha) + x5);
+f3 = eq2*(heaviside(s-x3) - heaviside(s-x4));
+
+fplot(f1 + f2 + f3 ,[0 x4+2])
+ylim([0 x5+2])
+
+eq1 = (x5/(x2-x1))*(s - x1);
+eq2 = (x5/(x4-x3))*(-s + x3) + x5;
+f1 = eq1*(heaviside(s-x1) - heaviside(s-x2));
+f2 = x5*(heaviside(s-x2)-heaviside(s-x3));
+f3 = eq2*(heaviside(s-x3) - heaviside(s-x4));
+figure
+fplot(f1 + f2 + f3 ,[0 x4+2])
+ylim([0 x5+2])
+
+
+
+
+
