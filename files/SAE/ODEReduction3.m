f74e0b6750f66f49da264319724ff8b5d92d6064
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-11-03 00:17:38 -0600

    Update ODEReduction3.m

--- files/SAE/ODEReduction3.m
+++ files/SAE/ODEReduction3.m
@@ -1,8 +1,7 @@
 close all; clear all; clc
 k0 = 1e-3;
-C0 = 0;
-% C0 = Tau0 - k0*S0; %Zero Initial Torsion
-s_span = [0 1];
+Tau0 = 0; %Zero Initial Torsion
+s_span = [0 10];
 for n=1:2
     c = rand(1,3);
     for i = 1:3
@@ -12,11 +11,11 @@ for n=1:2
     % Thus, 12 ICs are needed. 
     %Random ICs
     IC1 = 0;
-    IC2 = .5*rand;
-    IC3 = .7*rand;
-    IC4 = .1*rand;
+    IC2 = rand;
+    IC3 = rand;
+    IC4 = rand;
     ICs = [IC1; IC2; IC3; IC4];
-    [s,x] = ode45(@(s,x) r1ODE(s,x,k0,C0),s_span,ICs);
+    [s,x] = ode45(@(s,x) r1ODE(s,x,k0,Tau0),s_span,ICs);
     figure(1)
     r = x(:,1);
     A{:,i} = r;
@@ -54,7 +53,7 @@ C0 = 0;
 k1 = 0.1;
 % C0 = Tau0*(k0*S0 + k1).^2; %Zero Initial Torsion
 
-s_span = [0 1];
+s_span = [0 10];
 for n = 1:2
       c = rand(1,3);
     for i = 1:3
@@ -64,9 +63,9 @@ for n = 1:2
      % Thus, 12 ICs are needed. 
      %Random ICs
     IC1 = 0;
-    IC2 = .5*rand;
-    IC3 = .7*rand;
-    IC4 = .1*rand;
+    IC2 = rand;
+    IC3 = rand;
+    IC4 = rand;
     ICs = [IC1; IC2; IC3; IC4];
     [s,x] = ode45(@(s,x) r2ODE(s,x,k0,k1,C0),s_span,ICs);
     figure(3)
@@ -100,11 +99,11 @@ xlabel('X');ylabel('Y'); zlabel('Z');
 %view(0,0)
  
 
-function dydt = r1ODE(s,x,k0,C0)
-    dydt = [x(2); x(3); x(4); -k0.^2.*x(3)-k0.*(k0.*s+C0).^2.*x(1)];
+function dydt = r1ODE(s,x,k0,Tau0)
+    dydt = [x(2); x(3); x(4); -k0.^2.*x(3)-k0.*(Tau0).^2.*x(1)];
 end
 
 function dydt = r2ODE(s,x,k0,k1,C0)
 dydt = [x(2); x(3); x(4); ...
-    -(k0*s+k1).^2.*x(3)-((k0*s+k1)).*(3*k0+(C0/(k0*s+k1).^2).^2  ).*x(1)];
+    -(k0*s+k1).^2.*x(3)-((k0*s+k1)).*(3*k0+(C0/(k0*s+k1).^2).^2).*x(1)];
 end
