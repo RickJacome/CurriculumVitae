bede34df750a652cffa0e6dbd249a0d865fc5f75
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-11-16 15:47:36 -0600

    Update direction.m

--- files/SAE/direction.m
+++ files/SAE/direction.m
@@ -1,5 +1,8 @@
 function [O1,O2] = direction(K2)
     %Orthogonal Shift Function 
      O1 = atand(K2(:,2)./K2(:,1));  %Angle in Degrees
-     O2 = O1 + 90;   % 90 Degrees
+     %O1 = atan2d(K2(:,2),K2(:,1)); 
+     %; + 360*(K2(:,2)<0);
+     O2 = O1 - 90;   % If traveling southbound
+     %O2 = O1 + 90;   % If traveling northbound
 end
