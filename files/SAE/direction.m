456ce50b072fd0fca1c3463ca29bacccd3277c2f
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-11-20 10:49:21 -0600

    Update direction.m

--- files/SAE/direction.m
+++ files/SAE/direction.m
@@ -3,6 +3,6 @@ function [O1,O2] = direction(K2)
      O1 = atand(K2(:,2)./K2(:,1));  %Angle in Degrees
      %O1 = atan2d(K2(:,2),K2(:,1)); 
      %; + 360*(K2(:,2)<0);
-     O2 = O1 - 90;   % If traveling southbound
-     %O2 = O1 + 90;   % If traveling northbound
+     %O2 = O1 - 90;   % If traveling southbound
+     O2 = O1 + 90;   % If traveling northbound
 end
