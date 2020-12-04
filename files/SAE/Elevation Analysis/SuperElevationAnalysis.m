6f612a155499f58acfeca803df70fa41fbc8b211
RickJacome <48185556+RickJacome@users.noreply.github.com>
2020-12-03 22:42:20 -0600

    Update SuperElevationAnalysis.m

--- files/SAE/Elevation Analysis/SuperElevationAnalysis.m
+++ files/SAE/Elevation Analysis/SuperElevationAnalysis.m
@@ -1,4 +1,4 @@
-clc; clear all; close all
+clc; clear all; close all; format compact;
 load('ElevationProfile.mat');
 x = ElevationProfile.HorDist; y = ElevationProfile.VertDist;
 x=x(2:end); y = y(2:end);
@@ -7,7 +7,7 @@ scatter(x,y,'*'); grid on;
 xlabel('Horizontal Distance (km)'); ylabel('Vertical Elevation (m)')
 %Multi-Linear LLS Approximation
 %vector of 1-D look-up table "x" points
-XI = linspace(min(x),max(x),8);
+XI = linspace(min(x),max(x),20);
 % obtain vector of 1-D look-up table "y" points
 YI = lsq_lut_piecewise( x, y, XI );
 figure; plot(x,y,'.',XI,YI,'+-'); grid on;
@@ -15,10 +15,12 @@ legend('experimental data','LUT points','location','best')
 title('Piecewise 1-D look-up table least square estimation')
 
 % Superelevation Angle
-Fi = atand( abs( (z2 - z1)) /abs( (y2 - y1)*1000) ); 
-Se = atand( abs( (z3 - z2)) /abs( (y3 - y2)*1000) );
-Th = atand( abs( (z4 - z3)) /abs( (y4 - y3)*1000) );
+n = numel(YI);
+Theta = zeros(1,n);
+for i = 1:2:n-2
+Theta(i) = atand( abs( (y(i+1) - y(i))) /abs( (x(i+1) - x(i))*1000) ); 
+end
 %Superelevation as a percentage
-e1 = 100*sind(Fi)
-e2 = 100*sind(Se)
-e3 = 100*sind(Th)
+e = 100*sind(Theta);
+e(e==0) = [];
+
