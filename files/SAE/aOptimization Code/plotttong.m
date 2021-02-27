0a37c8ff48fb3c05f4d2fdf9611656b8fa9c7097
RickJacome <48185556+RickJacome@users.noreply.github.com>
2021-02-27 12:15:44 -0600

    Update plotttong.m

--- files/SAE/aOptimization Code/plotttong.m
+++ files/SAE/aOptimization Code/plotttong.m
@@ -10,7 +10,28 @@ plot(s, OpMu3E4,'linewidth',2);
 plot(s, OpMu6E4,'linewidth',2); 
 plot(s, OpMu8E4,'--','linewidth',2);
 
-legend('\mu = 0.1','\mu = 0.3','\mu = 0.6','\mu = 0.8')
+legend('\mu = 0.1','\mu = 0.3',...
+    '\mu = 0.6','\mu = 0.8','location','bestoutside')
+xlim([0 550]);
+ylim([25 40]); grid on;
+
+xlabel('Segment Length (m)');
+ylabel('Optimized Speed (m/s)');
+
+
+load('OpVeDMu8E4.mat'); OpVeDMu8E4 = Op(:,2);
+load('OpVeDMu6E4.mat'); OpVeDMu6E4 = Op(:,2);
+load('OpVeDMu3E4.mat'); OpVeDMu3E4 = Op(:,2);
+load('OpVeDMu1E4.mat'); OpVeDMu1E4 = Op(:,2);
+figure
+s = linspace(0,632,numel(OpMu8E4));
+plot(s, OpVeDMu1E4,'linewidth',2);  hold on;
+plot(s, OpVeDMu3E4,'linewidth',2);
+plot(s, OpVeDMu6E4,'linewidth',2); 
+plot(s, OpVeDMu8E4,'--','linewidth',2);
+
+legend('\mu = 0.1','\mu = 0.3',...
+    '\mu = 0.6','\mu = 0.8','location','bestoutside')
 xlim([0 550]);
 ylim([25 40]); grid on;
 
