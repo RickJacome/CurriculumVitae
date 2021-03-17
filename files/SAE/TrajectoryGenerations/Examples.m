09aff27fd20ddf802c53c6d14bc72f7bfcc0a06e
RickJacome <48185556+RickJacome@users.noreply.github.com>
2021-03-17 15:27:07 -0500

    Update Examples.m

--- files/SAE/TrajectoryGenerations/Examples.m
+++ files/SAE/TrajectoryGenerations/Examples.m
@@ -30,7 +30,10 @@ xlabel('X')
 ylabel('Y')
 grid on
 hold on
-
+%%
+% In a loop, step through the trajectory to output the current position 
+% and current orientation. Plot the current position and log the orientation.
+% Use pause to mimic real-time processing.
 orientationLog = zeros(toa(end)*trajectory.SampleRate,1,'quaternion');
 count = 1;
 while ~isDone(trajectory)
@@ -42,7 +45,9 @@ while ~isDone(trajectory)
    count = count + 1;
 end
 hold off
-
+%%
+% Convert the orientation quaternions to Euler angles for easy
+% interpretation, and then plot orientation over time.
 figure(2)
 eulerAngles = eulerd([orientation(1);orientationLog],'ZYX','frame');
 plot(toa,eulerAngles(:,1),'ko', ...
@@ -54,9 +59,19 @@ xlabel('Time (seconds)')
 ylabel('Rotation (degrees)')
 grid on
 
+%%
+%So far, the trajectory object has only output the waypoints that were 
+%specified during construction. To interpolate between waypoints,
+%increase the sample rate to a rate faster than the time of arrivals
+%of the waypoints. Set the trajectory sample rate to 100 Hz and call reset.
+
 trajectory.SampleRate = 100;
 reset(trajectory)
 %%
+%Create a figure and plot the initial position of the platform.
+%In a loop, step through the trajectory to output the current position
+%and current orientation. Plot the current position and log the 
+%orientation. Use pause to mimic real-time processing.
 
 figure(1)
 plot(waypoints(1,1),waypoints(1,2),'b*')
@@ -80,6 +95,13 @@ while ~isDone(trajectory)
 end
 hold off
 
+%The trajectory output now appears circular. This is because 
+%the waypointTrajectory System object™ minimizes the acceleration 
+%and angular velocity when interpolating, which results in smoother,
+%more realistic motions in most scenarios.
+%Convert the orientation quaternions to Euler angles for easy
+%interpretation, and then plot orientation over time. The orientation 
+%is also interpolated.
 %%
 figure(2)
 eulerAngles = eulerd([orientation(1);orientationLog],'ZYX','frame');
@@ -92,6 +114,12 @@ legend('Rotation around Z-axis','Rotation around Y-axis','Rotation around X-axis
 xlabel('Time (seconds)')
 ylabel('Rotation (degrees)')
 grid on
+%The waypointTrajectory algorithm interpolates the waypoints to 
+%create a smooth trajectory. To return to the square trajectory, 
+%provide more waypoints, especially around sharp changes. To track
+%corresponding times, waypoints, and orientation, specify all the
+%trajectory info in a single matrix.
+
 %%
         % Time, Waypoint, Orientation
 trajectoryInfo = [0,   0,0,0,    0,0,0; ... % Initial position
@@ -116,3 +144,48 @@ trajectory = waypointTrajectory(trajectoryInfo(:,2:4), ...
     'TimeOfArrival',trajectoryInfo(:,1), ...
     'Orientation',quaternion(trajectoryInfo(:,5:end),'eulerd','ZYX','frame'), ...
     'SampleRate',100);
+%Create a figure and plot the initial position of the platform.
+%In a loop, step through the trajectory to output the current
+%position and current orientation. Plot the current position and 
+%log the orientation. Use pause to mimic real-time processing.
+%%
+figure(1)
+plot(waypoints(1,1),waypoints(1,2),'b*')
+title('Position')
+axis([-1,2,-1,2])
+axis square
+xlabel('X')
+ylabel('Y')
+grid on
+hold on
+
+orientationLog = zeros(toa(end)*trajectory.SampleRate,1,'quaternion');
+count = 1;
+while ~isDone(trajectory)
+   [currentPosition,orientationLog(count)] = trajectory();
+
+   plot(currentPosition(1),currentPosition(2),'bo')
+
+   pause(trajectory.SamplesPerFrame/trajectory.SampleRate)
+   count = count+1;
+end
+hold off
+%The trajectory output now appears more square-like, 
+%especially around the vertices with waypoints.
+%Convert the orientation quaternions to Euler angles 
+%for easy interpretation, and then plot orientation over time.
+
+figure(2)
+eulerAngles = eulerd([orientation(1);orientationLog],'ZYX','frame');
+t = 0:1/trajectory.SampleRate:4;
+eulerAngles = plot(t,eulerAngles(:,1),'ko', ...
+                   t,eulerAngles(:,2),'bd', ...
+                   t,eulerAngles(:,3),'r.');
+title('Orientation Over Time')
+legend('Rotation around Z-axis', ...
+       'Rotation around Y-axis', ...
+       'Rotation around X-axis', ...
+       'Location', 'SouthWest')
+xlabel('Time (seconds)')
+ylabel('Rotation (degrees)')
+grid on
